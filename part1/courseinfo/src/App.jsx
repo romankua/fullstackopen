44776cabac18f4b3bda8c42bfcd4442931abd39d
Roman.K <akupaka@gmail.com>
2026-06-17 19:10:04 +0300

    Ex. 1.2 Refactor Content into subcomponents Part.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,18 +6,21 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
-        {props.part1} {props.exercises1}
+        {props.title} {props.exercises}
       </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+    </>
+  )
+}
+const Content = (props) => {
+  return (
+    <>
+      <Part title={props.part1} exercises={props.exercises1} />
+      <Part title={props.part2} exercises={props.exercises2} />
+      <Part title={props.part3} exercises={props.exercises3} />
     </>
   )
 }
