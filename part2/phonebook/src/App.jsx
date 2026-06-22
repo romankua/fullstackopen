3c0e471abdbfca359201e60afdc3054adb56e427
Roman.K <akupaka@gmail.com>
2026-06-22 03:04:41 +0300

    Ex. 2.9 Implement a search feature.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import NewPersonForm from './components/NewPersonForm'
 import PersonsList from './components/PersonsList'
+import Filter from './components/Filter'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '044-123-45-67'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  const [filter, setFilter] = useState('')
   
   const handleAddPerson = ({ name, number }) => {
     if (persons.findIndex(p => p.name.toLowerCase() === name.toLowerCase()) !== -1) {
@@ -29,8 +32,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter value={filter} onChange={setFilter}/>
       <NewPersonForm addPerson={handleAddPerson} />
-      <PersonsList persons={persons} />
+      <PersonsList persons={persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))} />
     </div>
   )
 }
--- part2/phonebook/src/components/NewPersonForm.jsx
+++ part2/phonebook/src/components/NewPersonForm.jsx
@@ -17,10 +17,10 @@ const NewPersonForm = ({ addPerson }) => {
       <h2>Add a new person</h2>
       <form onSubmit={handleAddName}>
         <div>
-          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+          name: <input value={newName} onChange={e => setNewName(e.target.value)} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)}/>
+          number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
