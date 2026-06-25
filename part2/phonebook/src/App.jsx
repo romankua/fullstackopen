981db6fa0bb011cc1c89a243586139bf343b4c68
Roman.K <akupaka@gmail.com>
2026-06-25 18:39:33 +0300

    Ex. 2.16-17 Add Notification component to Phonebook application.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,36 +3,55 @@ import personService from './services/persons'
 import NewPersonForm from './components/NewPersonForm'
 import PersonsList from './components/PersonsList'
 import DisplayOptions from './components/DisplayOptions'
+import Notification from './components/Notification'
 import './index.css'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [filter, setFilter] = useState('')
+  // {message: "Some text", category: "info|success|error"}
+  const [notification, setNotification] = useState({})
   
   const handleError = (error) => {
-    console.log("Request failed: ", error.message)
+    console.log("Error: ", error.message)
 
-    alert("Error happened, try refreshing browser window and repeat the operation.")
+    setNotification({message: `Error: ${error.message}. Refresh the page and try again.`, category: "error"})
+  }
+
+  const notifySuccess = (message) => {
+    setNotification({ message, category: "success" });
+    setTimeout(() => {
+      setNotification({});
+    }, 2000);
   }
 
   const loadPersons = () => {
     personService
       .getAll()
-      .then(data => setPersons(data))
+      .then(data => {
+        setPersons(data)
+        notifySuccess("Data loaded")
+      })
       .catch(error => handleError(error))
   }
 
   const addPerson = (person) => {
     personService
       .add(person)
-      .then(data => setPersons(persons.concat(data)))
+      .then(data => {
+        setPersons(persons.concat(data))
+        notifySuccess(`Added ${person.name}`)
+      })
       .catch(error => handleError(error))
   }
 
   const updatePerson = (person) => {
     personService
       .update(person)
-      .then(data => setPersons(persons.map(p => p.id === data.id ? data : p)))
+      .then(data => {
+        setPersons(persons.map(p => p.id === data.id ? data : p))
+        notifySuccess(`Updated ${person.name}`)
+      })
       .catch(error => handleError(error))
   }
 
@@ -49,7 +68,10 @@ const App = () => {
 
     personService
       .remove(id)
-      .then(() => setPersons(persons.filter(p => p.id !== id)))
+      .then(() => {
+        setPersons(persons.filter(p => p.id !== id))
+        notifySuccess(`Removed ${person.name}`)
+      })
       .catch(error => handleError(error))
   }
 
@@ -85,6 +107,7 @@ const App = () => {
   return (
     <div className='container col-6'>
       <h1 className='app-title'>Phonebook</h1>
+      <Notification message={notification.message} category={notification.category} />
       <NewPersonForm addPerson={handleAddPerson} />
       <DisplayOptions value={filter} onChange={setFilter}/>
       <PersonsList 
